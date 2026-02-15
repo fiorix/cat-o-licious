ca42a9352015b58c035a30e506005901bbc1d85c
Thiago Camargo <thiagocmc@proton.me>
2026-02-15 23:40:13 +0100

    refactor(wasm): fix `js.Func` memory leak in image loader (#15)
    Refactored the `media.NewImage` function to prevent a memory leak
    caused by unreleased `js.Func` callbacks.
    
    Unreleased `js.Func` objects prevent Go from garbage collecting their
    associated resources, which can degrade the stability of a WebAssembly
    application over time.
    
    This change introduces a `finalize` closure, protected by `sync.Once`,
    to ensure that all five event handler functions (`onload`, `onabort`,
    `onstalled`, `onsuspend`, `onerror`) are reliably released and detached
    from the JavaScript Image object. This brings the image loading logic in
    line with the robust resource management pattern already used in
    `media.NewAudio`.

--- wasm/media/image.go
+++ wasm/media/image.go
@@ -3,6 +3,7 @@ package media
 import (
 	"fmt"
 	"strconv"
+	"sync"
 	"syscall/js"
 )
 
@@ -18,34 +19,62 @@ func NewImage(uri, alt string) (Image, error) {
 
 	errc := make(chan error, 1)
 
-	img.Set("onload", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		close(errc)
-		return nil
-	}))
+	var once sync.Once
+	var onLoad, onAbort, onStalled, onSuspend, onError js.Func
 
-	img.Set("onabort", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		errc <- fmt.Errorf("aborted: %s", uri)
-		close(errc)
-		return nil
-	}))
+	finalize := func(err error) {
+		once.Do(func() {
+			// Detach handlers BEFORE releasing funcs to prevent JS from calling a released func.
+			img.Set("onload", js.Null())
+			img.Set("onabort", js.Null())
+			img.Set("onstalled", js.Null())
+			img.Set("onsuspend", js.Null())
+			img.Set("onerror", js.Null())
 
-	img.Set("onstalled", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		errc <- fmt.Errorf("stalled: %s", uri)
-		close(errc)
-		return nil
-	}))
+			// Release all js.Func resources.
+			onLoad.Release()
+			onAbort.Release()
+			onStalled.Release()
+			onSuspend.Release()
+			onError.Release()
 
-	img.Set("onsuspend", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		errc <- fmt.Errorf("suspended: %s", uri)
-		close(errc)
-		return nil
-	}))
+			if err != nil {
+				errc <- err
+			}
+			close(errc)
+		})
+	}
 
-	img.Set("onerror", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		errc <- fmt.Errorf("failed: %s", uri)
-		defer close(errc)
+	onLoad = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		finalize(nil)
 		return nil
-	}))
+	})
+
+	onAbort = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		finalize(fmt.Errorf("aborted: %s", uri))
+		return nil
+	})
+
+	onStalled = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		finalize(fmt.Errorf("stalled: %s", uri))
+		return nil
+	})
+
+	onSuspend = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		finalize(fmt.Errorf("suspended: %s", uri))
+		return nil
+	})
+
+	onError = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		finalize(fmt.Errorf("failed: %s", uri))
+		return nil
+	})
+
+	img.Set("onload", onLoad)
+	img.Set("onabort", onAbort)
+	img.Set("onstalled", onStalled)
+	img.Set("onsuspend", onSuspend)
+	img.Set("onerror", onError)
 
 	img.Set("src", uri)
 
